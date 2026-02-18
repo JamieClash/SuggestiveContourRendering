b1170dbfc4300d04cb8356a57240afd051e8bb72
Jamie Yeung <jamiethy@gmail.com>
2026-02-18 13:47:56 +0000

    look at camera implemented

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import numpy as np
 
 # load + center mesh
 tri_mesh = trimesh.load("skull.obj")
+tri_mesh.apply_translation(-tri_mesh.centroid)
+
 mesh = pyrender.Mesh.from_trimesh(tri_mesh)
-mesh_pose = np.eye(4)
+mesh_pose = np.array([
+    [1,0,0,0],
+    [0,0,1,0],
+    [0,-1,0,0],
+    [0,0,0,1]
+])
 
 # scene setup
 scene = pyrender.Scene(bg_color=[0,0,0,0])
@@ -18,13 +25,29 @@ light = pyrender.DirectionalLight()
 bbox = tri_mesh.bounding_box.extents
 scale = np.max(bbox)
 camera_distance = scale * 2
-cam_pose = np.array([
-    [1,0,0,0],
-    [0,1,0,0],
-    [0,0,1,camera_distance],
-    [0,0,0,1]
-])
 
+def look_at(target, eye, up):
+    forward = target - eye
+    forward /= np.linalg.norm(forward)
+
+    right = np.cross(forward, up)
+    right /= np.linalg.norm(right)
+
+    up = np.cross(right, forward)
+    
+    pose = np.eye(4)
+    pose[:3, 0] = right
+    pose[:3, 1] = up
+    pose[:3, 2] = -forward
+    pose[:3, 3] = eye
+
+    return pose
+
+origin = np.zeros(3)
+eye = np.array([0, 0, camera_distance])
+up = np.array([0, 1, 0])
+
+cam_pose = look_at(origin, eye, up)
 scene.add(camera, pose=cam_pose)
 scene.add(light, pose=cam_pose)
 
